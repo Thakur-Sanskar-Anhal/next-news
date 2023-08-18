2a831aeb06fc09a094987b6fbd28bf20120749d2
Sanskar <thakursanskaranhal@gmail.com>
2023-08-18 22:14:40 +0530

    did some work with bootstrap

--- src/components/News.js
+++ src/components/News.js
@@ -4,14 +4,19 @@ import NewsItem from './NewsItem'
 export class News extends Component {
   render() {
     return (
-      <div>
-        This is a news component
-        <NewsItem title="my title" description="my description"/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
+      <div className='container my3'>
+        <h2>Next.news top headlines </h2>
+        <div className="row my2">
+          <div className="col-md-4">
+            <NewsItem title="my title" description="my description"/> 
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="my title" description="my description"/> 
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="my title" description="my description"/> 
+          </div>
+        </div>
       </div>
     )
   }
